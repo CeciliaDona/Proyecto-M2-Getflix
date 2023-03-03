099c2222c35d7cd99b52747c9057dc38887d6b67
giselacano <velvetpro25@gmail.com>
2023-03-03 00:25:15 -0300

    cambie nombre en js y html

--- js/signUp.js
+++ js/signUp.js
@@ -1,19 +1,19 @@
-// const form = document.querySelector('#form');
-// const campo_email = document.querySelector('#User-email');
+// const form = document.querySelector('#form2');
+// const campo_email-1 = document.querySelector('#User-email-1');
 // const campo_name2 = document.querySelector('#User-name2');
-// const campo_name = document.querySelector('#User-name');
+// const campo_name1 = document.querySelector('#User-name1');
 // const campo_Password = document.querySelector('#User-Password');
 // const campo_Password3 = document.querySelector('#User-Password3');
 
-// const alert_name = document.querySelector('#alert-name');
-// const alert_email = document.querySelector('#alert-email');
+// const alert_name1 = document.querySelector('#alert-name1');
+// const alert_email-1 = document.querySelector('#alert-email-1');
 // const alert_success = document.querySelector('#alert-success');
 // const alert_name2 = document.querySelector('#alert-name2');
 // const alert_Password = document.querySelector('#alert-Password');
 // const alert_Password3 = document.querySelector('#alert-Password3');
 
-// const regUsername = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-// const regUseremail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+// const regUsername1 = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+// const regUseremail-1 = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
 // const regUsername2 = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 // const regUserPassword = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 // const regUserPassword3 =  /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
@@ -36,28 +36,28 @@
 
 // 	const errores = [];
 
-// 	if (!regUsername.test(campo_name.value) || !campo_name.value.trim()) {
-// 		campo_name.classList.add('is-invalid');
+// 	if (!regUsername1.test(campo_name1.value) || !campo_name1.value.trim()) {
+// 		campo_name1.classList.add('is-invalid');
 // 		errores.push({
-// 			tipo: alert_name,
+// 			tipo: alert_name1,
 // 			msg: 'Invalid characters,only letters.',
 // 		});
 // 	} else {
-// 		alert_name.classList.add('d-none');
-// 		campo_name.classList.add('is-valid');
-// 		campo_name.classList.remove('is-invalid');
+// 		alert_name1.classList.add('d-none');
+// 		campo_name1.classList.add('is-valid');
+// 		campo_name1.classList.remove('is-invalid');
 // 	}
 
-// 	if (!regUseremail.test(campo_email.value) || !campo_email.value.trim()) {
-// 		campo_email.classList.add('is-invalid');
+// 	if (!regUseremail-1.test(campo_email-1.value) || !campo_email-1.value.trim()) {
+// 		campo_email-1.classList.add('is-invalid');
 // 		errores.push({
-// 			tipo: alert_email,
+// 			tipo: alert_email-1,
 // 			msg: 'Invalid email.',
 // 		});
 // 	} else {
-// 		alert_email.classList.add('d-none');
-// 		campo_email.classList.add('is-valid');
-// 		campo_email.classList.remove('is-invalid');
+// 		alert_email-1.classList.add('d-none');
+// 		campo_email-1.classList.add('is-valid');
+// 		campo_email-1.classList.remove('is-invalid');
 // 	}
 //     if (!regUsername2.test(campo_name2.value) || !campo_name2.value.trim()) {
 // 		campo_name2.classList.add('is-invalid');
@@ -71,15 +71,15 @@
 // 		campo_name2.classList.remove('is-invalid');
 // 	}
 
-//     if (!regUserPassword.test(campo_Password.value) || !campo_Password.value.trim()) {
-// 		campo_Password.classList.add('is-invalid');
+//     if (!regUserPassword3.test(campo_Password3.value) || !campo_Password3.value.trim()) {
+// 		campo_Password3.classList.add('is-invalid');
 // 		errores.push({
-// 			tipo: alert_Password,
+// 			tipo: alert_Password3,
 // 		});
 // 	} else {
-// 		alert_Password.classList.add('d-none');
-// 		campo_Password.classList.add('is-valid');
-// 		campo_Password.classList.remove('is-invalid');
+// 		alert_Password3.classList.add('d-none');
+// 		campo_Password3.classList.add('is-valid');
+// 		campo_Password3.classList.remove('is-invalid');
 // 	}
 //     if (!regUserPassword3.test(campo_Password3.value) || !campo_Password3.value.trim()) {
 // 		campo_Password3.classList.add('is-invalid');
