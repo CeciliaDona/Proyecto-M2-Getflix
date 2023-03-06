9dc70b6d1bc360a833f2d6f01db6eddc62be012a
Serena Lin <116319416+serena01lin@users.noreply.github.com>
2023-03-05 23:11:53 -0300

    admin rol

--- js/admin1.js
+++ js/admin1.js
@@ -1,1 +1,18 @@
-console.log("probando");
+const open = document.querySelectorAll('#open');
+const close = document.querySelector('#btn-close');
+const modal = document.querySelector('.modal__container');
+const add = document.querySelector('.bi-plus-circle-fill');
+const remove = document.querySelectorAll('.bi-trash3-fill');
+const edit = document.querySelectorAll('#edit');
+
+let moviesArray = [];
+
+open.forEach((item) => {
+    item.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+});
+
+close.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
