6b80fcaee586e40c58eba5bbc4afee673219f18f
Serena Lin <116319416+serena01lin@users.noreply.github.com>
2023-03-06 19:05:07 -0300

    agregue js de los clicks

--- js/admin1.js
+++ js/admin1.js
@@ -4,8 +4,11 @@ const modal = document.querySelector('.modal__container');
 const add = document.querySelector('.bi-plus-circle-fill');
 const remove = document.querySelectorAll('.bi-trash3-fill');
 const edit = document.querySelectorAll('#edit');
+const back = document.querySelector('#back');
+const addMovie = document.querySelector('.modal__modification');
+const table = document.querySelector('.modal__table');
 
-let moviesArray = [];
+let moviesArray = Array.from(document.querySelectorAll('.modal__table-titles'));
 
 open.forEach((item) => {
     item.addEventListener('click', function() {
@@ -16,3 +19,18 @@ open.forEach((item) => {
 close.addEventListener('click', function() {
     modal.style.display = 'none';
 });
+
+add.addEventListener('click', function() {
+    table.style.display = 'none';
+    close.style.display = 'none';
+    add.style.display = 'none';
+    addMovie.style.display = 'flex';
+});
+
+back.addEventListener('click', function(){
+    table.style.display = 'flex';
+    close.style.display = 'flex';
+    add.style.display = 'flex';
+    addMovie.style.display = 'none';
+});
+
