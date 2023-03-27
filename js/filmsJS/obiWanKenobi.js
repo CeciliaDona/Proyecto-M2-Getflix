1f7ce04661da0d00868036c3db7faa013c2de0ae
Cecilia Do Nascimento <mcd.cecilia.donascimento@gmail.com>
2023-03-27 00:24:34 -0300

    corregí links del navbar

--- js/filmsJS/obiWanKenobi.js
+++
@@ -1,8 +0,0 @@
-// sign out
-
-const signOutButton = document.getElementById('sign-out-btn');
-
-signOutButton.addEventListener('click', (e) => {
-	e.preventDefault();
-	window.location.href = '../../index.html'; // reemplaza "ejemplo.com" con la URL de tu página de inicio
-});
