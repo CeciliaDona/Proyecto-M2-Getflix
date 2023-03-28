3ed9d022929eefe9387616120498b8a47ce5ffae
Cecilia Do Nascimento <mcd.cecilia.donascimento@gmail.com>
2023-03-27 21:27:13 -0300

    estilos contact finalizado

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,7 @@
-const form = document.querySelector('#formulario');
+const form = document.querySelector('#form');
 const campo_email = document.querySelector('#User-email');
-const campo_phone = document.querySelector('#User-phone');
-const campo_name = document.querySelector('#User-name');
-const campo_message = document.querySelector('#User-message');
+const input_phone = document.querySelector('#User-phone');
+const input_name = document.querySelector('#User-name');
 const alert_name = document.querySelector('#alert-name');
 const alert_email = document.querySelector('#alert-email');
 const alert_success = document.querySelector('#alert-success');
@@ -12,36 +11,36 @@ const regUsername = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 const regUseremail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
 const regUserphone = /^\d+$/gi;
 
-const mensajeExito = () => {
+const msjSuccess = () => {
 	alert_success.textContent = 'The form has been sent successfully';
 	alert_success.classList.remove('d-none');
 }
-const pintarMensajeError = (errores) => {
-	errores.forEach((item) => {
+const errorMsj = (errors) => {
+	errors.forEach((item) => {
 		item.tipo.classList.remove('d-none');
 		item.tipo.textContent = item.msg;
 	});
 }
 
-formulario.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	alert_success.classList.add('d-none');
 
-	const errores = [];
-	if (!regUsername.test(campo_name.value) || !campo_name.value.trim()) {
-		campo_name.classList.add('is-invalid');
-		errores.push({
+	const errors = [];
+	if (!regUsername.test(input_name.value) || !input_name.value.trim()) {
+		input_name.classList.add('is-invalid');
+		errors.push({
 			tipo: alert_name,
 			msg: 'Invalid characters, only letters.',
 		});
 	} else {
 		alert_name.classList.add('d-none');
-		campo_name.classList.add('is-valid');
-		campo_name.classList.remove('is-invalid');
+		input_name.classList.add('is-valid');
+		input_name.classList.remove('is-invalid');
 	}
 	if (!regUseremail.test(campo_email.value) || !campo_email.value.trim()) {
 		campo_email.classList.add('is-invalid');
-		errores.push({
+		errors.push({
 			tipo: alert_email,
 			msg: 'Invalid email.',
 		});
@@ -50,20 +49,20 @@ formulario.addEventListener('submit', (e) => {
 		campo_email.classList.add('is-valid');
 		campo_email.classList.remove('is-invalid');
 	}
-    if (!regUserphone.test(campo_phone.value) || !campo_phone.value.trim()) {
-		campo_phone.classList.add('is-invalid');
-		errores.push({
+    if (!regUserphone.test(input_phone.value) || !input_phone.value.trim()) {
+		input_phone.classList.add('is-invalid');
+		errors.push({
 			tipo: alert_phone,
 			msg: 'Invalid phone number, only numbers.',
 		});
 	} else {
 		alert_phone.classList.add('d-none');
-		campo_phone.classList.add('is-valid');
-		campo_phone.classList.remove('is-invalid');
+		input_phone.classList.add('is-valid');
+		input_phone.classList.remove('is-invalid');
 	}
-	if (errores.length !== 0) {
-		pintarMensajeError(errores);
+	if (errors.length !== 0) {
+		errorMsj(errors);
 		return;
 	}
-	mensajeExito();
+	msjSuccess();
 });
